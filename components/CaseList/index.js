a1f716618a983fc307fe8a3a6fbaa0d97effbc3d
byfabioalencar@gmail.com <byfabioalencar@gmail.com>
2021-02-21 21:29:03 -0300

    Fixing links link component

--- components/CaseList/index.js
+++ components/CaseList/index.js
@@ -1,27 +1,33 @@
 import React from "react";
 import Container from "./styles";
 
+import Link from "next/link";
+
 export default function CaseList() {
   return (
     <Container>
       <ul id="cases">
         <li className="trinks">
-          <a href="/cases/trinks">
-            <div>
-              <h2>Find and book beauty services with Trinks</h2>
-              <p>Product Design Leadership and Design Ops at Trinks</p>
-            </div>
-            <span>View case</span>
-          </a>
+          <Link href="/cases/trinks">
+            <a>
+              <div>
+                <h2>Find and book beauty services with Trinks</h2>
+                <p>Product Design Leadership and Design Ops at Trinks</p>
+              </div>
+              <span>View case</span>
+            </a>
+          </Link>
         </li>
         <li className="trinks">
-          <a href="/cases/trinks">
-            <div>
-              <h2>Find and book beauty services with Trinks</h2>
-              <p>Product Design Leadership and Design Ops at Trinks</p>
-            </div>
-            <span>View case</span>
-          </a>
+          <Link href="/cases/trinks">
+            <a>
+              <div>
+                <h2>Find and book beauty services with Trinks</h2>
+                <p>Product Design Leadership and Design Ops at Trinks</p>
+              </div>
+              <span>View case</span>
+            </a>
+          </Link>
         </li>
       </ul>
     </Container>
